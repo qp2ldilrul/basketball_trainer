404f48391bc8ec81b3751915f80af0441469fcf1
qp2ldilrul <qp2ldilrul@users.noreply.github.com>
2026-05-01 17:17:12 +0000

    deploy: 69a32c7fc107a25e0755a902424f10008daaa796

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3455098594" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "224061076" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
